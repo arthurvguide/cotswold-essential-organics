7d173d53a5e89ddc212285f6e4a565e2728fd093
arthurvguide <arthurvguideuk@gmail.com>
2022-03-23 10:10:43 +0000

    Create index.htm and extend base.html

--- cwessentialorg/urls.py
+++ cwessentialorg/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import get_base
+from main.views import get_base, get_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', get_base, name='base'),
+    path('', get_home, name='home'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,6 @@ from django.views import View
 # Create your views here.
 def get_base(request):
     return render(request,'base.html')
+
+def get_home(request):
+    return render(request,'home/index.html')
