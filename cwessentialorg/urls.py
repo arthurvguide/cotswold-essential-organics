d96aa5efea667799a56a34bc94b3944172092140
arthurvguide <arthurvguideuk@gmail.com>
2022-03-23 11:52:01 +0000

    Install and setup allauth

--- cwessentialorg/urls.py
+++ cwessentialorg/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main.views import get_base, get_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('base/', get_base, name='base'),
     path('', get_home, name='home'),
 ]
